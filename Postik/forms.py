cb6466e575971eecc478d458a3cb7a630e158afa
SpaikerZ <egori228337@gmail.com>
2021-06-16 20:46:07 +0300

    0.0.4(forms)

--- Postik/forms.py
+++ Postik/forms.py
@@ -3,6 +3,8 @@ from .models import postmodel
 
 class postform(forms.ModelForm):
 
+    NamePost = forms.CharField(max_length = 15)
+
     class Meta:
 
         fields = ('NamePost', 'TextPost', 'ConfirmId' )
--- Postik/views.py
+++ Postik/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from . import forms
-from . import models
+from .forms import postform
+from .models import postmodel
 
 # Create your views here.
 
@@ -11,7 +11,7 @@ def index(request):
 def create(request):
     
     if request.method == 'POST':
-        form = createFrom(request.POST)
+        form = postform(request.POST)
 
         if form.is_valid():
             task = form.save(commit=False)
@@ -22,7 +22,8 @@ def create(request):
         else:
             return HttpResponse("<h1>Form not valid </h1><h2>Or something was wrong</h2>")
     
-    return render(request, 'Postik/create.html')
+    else:
+        return render(request, 'Postik/create.html')
 
 
 
